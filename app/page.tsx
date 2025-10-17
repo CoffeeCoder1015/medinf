4bcff6d0a721aba36406b185f6160bf141fa04bd
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-10-17 11:58:04 -0700

    color change

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
 
   function MedicineContext() {
     return ( 
-      <Card className="w-4xl">
+      <Card className="w-4xl bg-gray-100">
         <CardContent className="ml-2 mr-2 flex flex-col gap-2">
-            <Button className="min-h-15">
+            <Button className="min-h-15" variant="outline">
               <Camera />
               Search by picture
             </Button>
